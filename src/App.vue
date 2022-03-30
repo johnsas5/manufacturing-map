f151d45381587d1372a91027335071495d1d4b6f
MCBaileyGitHub <MCBAileyGitHub@gmail.com>
2022-03-30 10:53:52 -0400

    Created basic login page

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <LoginPage />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from "vue-property-decorator";
+// import HelloWorld from './components/HelloWorld.vue';
+import LoginPage from "./components/LoginPage.vue";
 
 @Component({
   components: {
-    HelloWorld,
+    LoginPage,
   },
 })
 export default class App extends Vue {}
