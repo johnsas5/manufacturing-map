471e8c63a2d84723a8196ac1f5f8d420ab15ec69
MCBaileyGitHub <MCBAileyGitHub@gmail.com>
2022-04-12 10:29:47 -0400

    First commit

---
+++ src/myconfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyDUa0pPt3VFFxOFyG82K3OZKVgnjUASDr8",
+  authDomain: "homework4-df2d7.firebaseapp.com",
+  projectId: "homework4-df2d7",
+  storageBucket: "homework4-df2d7.appspot.com",
+  messagingSenderId: "969582307205",
+  appId: "1:969582307205:web:9010cc7b7651e2636886ec",
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
